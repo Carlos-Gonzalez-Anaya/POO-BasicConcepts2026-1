6de98e920629a81c997151f39805077334daabd6
CARLOS GONZALEZ ANAYA <carlosgonzalesanaya@gmail.com>
2026-03-08 18:53:31 -0500

    Creation of an OOP frontend and backend project

---
+++ POO-BasicConcepts2026-1/POO-BasicConcepts2026-1.Frontend/Program.cs
@@ -0,0 +1,18 @@
+﻿using POO_BasicConcepts2026_1.Backend;
+
+
+try
+{
+    var date1 = new Date();
+    var date2 = new Date(2028, 2, 28);
+    var date3 = new Date(2012, 11, 30);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
